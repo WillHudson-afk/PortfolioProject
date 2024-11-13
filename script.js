3579bcf41bc0d42b883b1d19e66947f20a12b16f
Will <willhudwork@gmail.com>
2024-11-13 12:39:15 +0000

    to be pushed to github

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleVisibility() {
+    var element = document.getElementById("toDoMoreInfo");
+    // Check the current display status and toggle it
+    if (element.style.display === "none") {
+      element.style.display = "block";  // Show the element
+    } else {
+      element.style.display = "none";   // Hide the element
+    }
+  }
+
+  function toggleVisibility2() {
+    var element = document.getElementById("luhnsMoreInfo");
+    // Check the current display status and toggle it
+    if (element.style.display === "none") {
+      element.style.display = "block";  // Show the element
+    } else {
+      element.style.display = "none";   // Hide the element
+    }
+  }
+
+  function toggleVisibility3() {
+    var element = document.getElementById("dnaMoreInfo");
+    // Check the current display status and toggle it
+    if (element.style.display === "none") {
+      element.style.display = "block";  // Show the element
+    } else {
+      element.style.display = "none";   // Hide the element
+    }
+  }
